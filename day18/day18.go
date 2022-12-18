2a6352efba395a8f538e957e95c41ec82627b1c2
Vincent De Rijcke <vincent.derijcke@bjss.com>
2022-12-18 16:58:23 +0000

    AOC2022 - Day 18 - Part 2

--- day18/day18.go
+++ day18/day18.go
@@ -12,6 +12,13 @@ import (
 //go:embed input.txt
 var input string
 
+const (
+	GRID_SIZE = 20 // Eyeballed in input
+	EMPTY     = ' '
+	AIR       = '.'
+	STONE     = '#'
+)
+
 func main() {
 	start := time.Now()
 	fmt.Println("Start", start)
@@ -28,8 +35,8 @@ func resolve(input string) (resultPart1 int, resultPart2 int) {
 	cubes := utils.SliceMap(lines, parseCube)
 
 	sortCubes(cubes)
-	grid := render(cubes)
-	resultPart1 = part1(grid)
+	resultPart1 = part1(render(cubes, AIR))
+	resultPart2 = part2(render(cubes, EMPTY))
 
 	return
 }
@@ -48,16 +55,13 @@ func sortCubes(cubes []*Cube) {
 	sort.Slice(cubes, byZYX)
 }
 
-func render(cubes []*Cube) [][][]bool {
-	grid := make([][][]bool, 20)
+func render(cubes []*Cube, r rune) [][][]rune {
+	grid := make([][][]rune, GRID_SIZE)
 	for z := 0; z < len(grid); z++ {
-		grid[z] = make([][]bool, 20)
-		for y := 0; y < len(grid[z]); y++ {
-			grid[z][y] = make([]bool, 20)
-		}
+		grid[z] = utils.GridRunes(r, GRID_SIZE, GRID_SIZE)
 	}
 	for _, cube := range cubes {
-		grid[cube.z][cube.y][cube.x] = true
+		grid[cube.z][cube.y][cube.x] = STONE
 	}
 
 	return grid
@@ -65,28 +69,28 @@ func render(cubes []*Cube) [][][]bool {
 
 type Cube struct{ x, y, z int }
 
-func part1(grid [][][]bool) int {
+func part1(grid [][][]rune) int {
 	surface := 0
 	for z, level := range grid {
 		for y, row := range level {
 			for x, cube := range row {
-				if cube {
-					if x == 0 || !grid[z][y][x-1] {
+				if cube == STONE {
+					if x == 0 || grid[z][y][x-1] == AIR {
 						surface++
 					}
-					if x == len(row)-1 || !grid[z][y][x+1] {
+					if x == len(row)-1 || grid[z][y][x+1] == AIR {
 						surface++
 					}
-					if y == 0 || !grid[z][y-1][x] {
+					if y == 0 || grid[z][y-1][x] == AIR {
 						surface++
 					}
-					if y == len(level)-1 || !grid[z][y+1][x] {
+					if y == len(level)-1 || grid[z][y+1][x] == AIR {
 						surface++
 					}
-					if z == 0 || !grid[z-1][y][x] {
+					if z == 0 || grid[z-1][y][x] == AIR {
 						surface++
 					}
-					if z == len(grid)-1 || !grid[z+1][y][x] {
+					if z == len(grid)-1 || grid[z+1][y][x] == AIR {
 						surface++
 					}
 				}
@@ -95,4 +99,49 @@ func part1(grid [][][]bool) int {
 	}
 
 	return surface
+}
+
+func part2(grid [][][]rune) int {
+	for filling, up := true, true; filling; up = !up {
+		filling = false
+		start, step := 0, 1
+		if !up {
+			start, step = len(grid)-1, -1
+		}
+		for z := start; z >= 0 && z < len(grid); z += step {
+			level := grid[z]
+			for y, row := range level {
+				for x, _ := range row {
+					if grid[z][y][x] == EMPTY {
+						if x == 0 || grid[z][y][x-1] == AIR {
+							grid[z][y][x] = AIR
+							filling = true
+						}
+						if x == len(row)-1 || grid[z][y][x+1] == AIR {
+							grid[z][y][x] = AIR
+							filling = true
+						}
+						if y == 0 || grid[z][y-1][x] == AIR {
+							grid[z][y][x] = AIR
+							filling = true
+						}
+						if y == len(level)-1 || grid[z][y+1][x] == AIR {
+							grid[z][y][x] = AIR
+							filling = true
+						}
+						if z == 0 || grid[z-1][y][x] == AIR {
+							grid[z][y][x] = AIR
+							filling = true
+						}
+						if z == len(grid)-1 || grid[z+1][y][x] == AIR {
+							grid[z][y][x] = AIR
+							filling = true
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return part1(grid)
 }
