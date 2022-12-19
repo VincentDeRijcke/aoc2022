e2fd6782621e7260c0309b3db1390f9e21aeced2
Vincent De Rijcke <vincent.derijcke@bjss.com>
2022-12-19 15:55:52 +0000

    AOC2022 - Day 19 - Part 1 SLLLLLOOOOW

--- day19/day19.go
+++ day19/day19.go
@@ -33,28 +33,38 @@ func resolve(input string) (resultPart1 int, resultPart2 int) {
 type Blueprint struct{ index, oreOre, clayOre, obsOre, obsClay, geodeOre, geodeObs int }
 
 func (bp *Blueprint) buildOre(c *Configuration, r *Resource) (*Configuration, *Resource) {
-	if bp.oreOre <= r.ore && c.ore < utils.Max(bp.oreOre, bp.clayOre, bp.obsOre, bp.geodeOre) {
+	if bp.oreOre <= r.ore && c.ore < bp.maxOre() {
 		return &Configuration{c.ore + 1, c.clay, c.obs, c.geode},
 			&Resource{r.ore - bp.oreOre, r.clay, r.obs, r.geode}
 	}
 
 	return nil, nil
 }
+func (bp *Blueprint) maxOre() int {
+	return utils.Max(bp.oreOre, bp.clayOre, bp.obsOre, bp.geodeOre)
+}
+
 func (bp *Blueprint) buildClay(c *Configuration, r *Resource) (*Configuration, *Resource) {
-	if bp.clayOre <= r.ore && c.clay < bp.obsClay {
+	if bp.clayOre <= r.ore && c.clay < bp.maxClay() {
 		return &Configuration{c.ore, c.clay + 1, c.obs, c.geode},
 			&Resource{r.ore - bp.clayOre, r.clay, r.obs, r.geode}
 	}
 
 	return nil, nil
 }
+func (bp *Blueprint) maxClay() int {
+	return bp.obsClay
+}
 func (bp *Blueprint) buildObs(c *Configuration, r *Resource) (*Configuration, *Resource) {
-	if bp.obsOre <= r.ore && bp.obsClay <= r.clay && c.obs < bp.geodeObs {
+	if bp.obsOre <= r.ore && bp.obsClay <= r.clay && c.obs < bp.maxObs() {
 		return &Configuration{c.ore, c.clay, c.obs + 1, c.geode},
 			&Resource{r.ore - bp.obsOre, r.clay - bp.obsClay, r.obs, r.geode}
 	}
 
 	return nil, nil
+}
+func (bp *Blueprint) maxObs() int {
+	return bp.geodeObs
 }
 func (bp *Blueprint) buildGeode(c *Configuration, r *Resource) (*Configuration, *Resource) {
 	if bp.geodeOre <= r.ore && bp.geodeObs <= r.obs {
@@ -94,8 +104,12 @@ func parse(line string) *Blueprint {
 
 func part1(bps []*Blueprint) int {
 	total := 0
-	for _, bp := range bps {
+	start := time.Now()
+	for i, bp := range bps {
 		total += bp.index * solveBp(bp)
+		bpTime := int64(time.Now().Sub(start)) / int64(i+1)
+		remaining := bpTime * int64(len(bps)-i-1)
+		fmt.Println("Remaining: ", time.Duration(remaining))
 	}
 
 	return total
@@ -106,14 +120,13 @@ func solveBp(bp *Blueprint) int {
 	fmt.Println("Result for ", *bp, " is ", max)
 	return max
 }
-
 func move(time int, conf *Configuration, res *Resource, bp *Blueprint) int {
 	if time == 24 {
 		return conf.tick(res).geode
 	}
 	max := 0
 	if c, r := bp.buildOre(conf, res); c != nil {
-		utils.Max(max, move(time+1, c, conf.tick(r), bp))
+		max = utils.Max(max, move(time+1, c, conf.tick(r), bp))
 	}
 	if c, r := bp.buildClay(conf, res); c != nil {
 		max = utils.Max(max, move(time+1, c, conf.tick(r), bp))
